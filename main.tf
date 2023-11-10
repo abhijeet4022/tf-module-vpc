8315e0b22e111ad1632ff0104ef177f15c78c4a8
abhijeet4022 <abhijeet4022@gmail.com>
2023-11-10 08:51:14 +0530

    Terraform: Roboshop-App Terraform

--- main.tf
+++ main.tf
@@ -30,12 +30,18 @@ resource "aws_route" "igw" {
 resource "aws_eip" "ngw" {
   for_each = lookup(lookup(module.subnets, "public", null), "subnets_ids", null)
   domain   = "vpc"
+  tags = {
+    Name = "a"
+  }
 }
 
 resource "aws_nat_gateway" "ngw" {
   for_each      = lookup(lookup(module.subnets, "public", null), "subnets_ids", null)
   allocation_id = lookup(aws_eip, each.value["id"], null)
   subnet_id     = each.value["id"]
+  tags = {
+    Name = "${each.key}-ngw"
+  }
 }
 
 output "subnet" {
