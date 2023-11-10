74f665cf41c6a7bb9e69a1f05a5df658028e86de
abhijeet4022 <abhijeet4022@gmail.com>
2023-11-10 08:53:24 +0530

    Terraform: Roboshop-App Terraform

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ resource "aws_eip" "ngw" {
 
 resource "aws_nat_gateway" "ngw" {
   for_each      = lookup(lookup(module.subnets, "public", null), "subnets_ids", null)
-  allocation_id = lookup(aws_eip, each.value["id"], null)
+  allocation_id = lookup(aws_eip.ngw, each.value["id"], null)
   subnet_id     = each.value["id"]
   tags = {
     Name = "${each.key}-ngw"
